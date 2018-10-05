2e8d0ff755da23166965aadb6260b894831d5e7e
ThomasFrau <32460955+ThomasFrau@users.noreply.github.com>
2018-10-05 12:39:10 +0100

    Added JSON framework

---
+++ TopDog/TopDog/GameScript.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDog
+{
+    /// <summary>
+    /// This contains all the stuff needed for the game to run.
+    /// </summary>
+    class GameScript
+    {
+        /// <summary>
+        /// Number of cards selected by the player
+        /// </summary>
+        private int tempCardNum = 4;// For debugging only, need to be changed so the layer can input it through the setup form.
+
+        dynamic jsonFile = Job
+
+    }
+}
